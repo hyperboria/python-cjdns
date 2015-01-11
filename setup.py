955768e567339e6a3a9d8442d8b7dc21d715721a
Finn <thefinn93@thefinn93.com>
2015-01-11 13:47:37 -0800

    Added a setup.py file, gitignore'd some dist stuff

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import os
+
+
+def readme(fname):
+    md = open(os.path.join(os.path.dirname(__file__), fname)).read()
+    output = md
+    try:
+        import pypandoc
+        output = pypandoc.convert(md, 'rst', format='md')
+    except ImportError:
+        pass
+    return output
+
+setup(
+    name='cjdnsadmin',
+    version='0.1.0a1',
+    description='A library to interact with the cjdns Admin Interface',
+    long_description=readme('README.md'),
+    url='https://github.com/thefinn93/cjdnsadmin',
+    author='Finn Herzfeld',
+    author_email='finn@seattlemesh.net',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Topic :: System :: Networking'
+    ],
+    keywords='cjdns',
+    packages=['cjdnsadmin'],
+    extra_require={
+        'package': ['pypandoc']
+    }
+)
