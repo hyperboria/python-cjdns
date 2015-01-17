66bbb444f1e28cfc58a6d9940d24fb67bfae7221
Finn <thefinn93@thefinn93.com>
2015-01-17 11:52:28 -0800

    little things

--- tests/cjdns.sh
+++ tests/cjdns.sh
@@ -6,6 +6,6 @@ cd cjdns
 
 echo "Generating, cleaning conf"
 ./cjdroute --genconf | ./cjdroute --cleanconf > ~/cjdroute.conf
-./cjdroute < cjdroute.conf
+./cjdroute < ~/cjdroute.conf
 python2 contrib/python/cjdnsadminmaker.py
 contrib/nodejs/tools/cexec.js 'ping()'
