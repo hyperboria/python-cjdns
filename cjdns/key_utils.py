6b5fe1b494476861cab6ddbd9c1cb441b9372cea
Johannes Löthberg <johannes@kyriasis.com>
2015-09-18 00:47:12 +0200

    key_utils: add get_ip6 function to get IP from key
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

---
+++ cjdns/key_utils.py
@@ -0,0 +1,28 @@
+# You may redistribute this program and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from hashlib import sha512
+
+import base32
+
+
+def get_ipv6(key):
+    """Get IPv6 address from a public key."""
+
+    if key[-2:] != '.k':
+        raise ValueError('Key does not end with .k')
+
+    key_bytes = base32.decode(key[:-2])
+    hash_one = sha512(key_bytes).digest()
+    hash_two = sha512(hash_one).hexdigest()
+
+    return ':'.join([hash_two[i:i+4] for i in range(0, 32, 4)])
